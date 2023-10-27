cf4d8a9cde583802dea28db49e7346f8931bb6a8
Felipe Paneque <paneque.malta@gmail.com>
2023-10-27 17:37:57 +0100

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-document.querySelector('')
+document.addEventListener('DOMContentLoaded', function () {
+    var modal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {
+        backdrop: false
+    });
+
+    var modalButton = document.getElementById('openModalBtn');
+
+    modalButton.addEventListener('click', function() {
+        modal.show();
+    });
+});
+
